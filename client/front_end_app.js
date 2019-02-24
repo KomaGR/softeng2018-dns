7a8cad4a0f288137f3ec2a4bb70dc19916588b33
GitZonk <vassilis_mitropoulos@yahoo.gr>
2019-02-24 19:22:18 +0200

    First iteration of the basic structure of our site. PENDING: remove routes from front_end_app.js and handle them on their own in the folder client_routes

--- client/front_end_app.js
+++ client/front_end_app.js
@@ -13,9 +13,32 @@ class Front_end_app {
         this.front_end_app.use(bodyParser.urlencoded({ extended: false }));
         routes_1.default(this.front_end_app);
         this.front_end_app.get("/", function(req, res){
-            res.render("../client/homepage.ejs");
+            res.render("../client/pages/homepage.ejs");
         });
-        
+        this.front_end_app.get("/search_results", function(req, res){
+            var searchTerm = req.query.search_term;
+            request("https://localhost:8765/observatory/api" + searchTerm, function(error, response, body){
+                if(error){
+                    console.log(error);
+                }else{
+                    res.render("../client/pages/search_results.ejs", {
+                        prodData: JSON.parse(body)
+                    });
+                }
+            });
+        });
+        this.front_end_app.get("/about", function(req, res){
+            res.render("../client/pages/about.ejs");
+        });
+        this.front_end_app.get("/login", function(req, res){
+            res.render("../client/pages/login.ejs");
+        });
+        this.front_end_app.get("/sign_up", function(req, res){
+            res.render("../client/pages/sign_up.ejs");
+        });
+        this.front_end_app.get("/submit_product", function(req, res){
+            res.render("../client/pages/submit_product.ejs");
+        });
     }
 }
 exports.default = new Front_end_app().front_end_app;
