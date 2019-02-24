ffb5fd6a65d2af9dd7310415c43f5240489a4964
GitZonk <vassilis_mitropoulos@yahoo.gr>
2019-02-24 16:27:24 +0200

    Created client side app.js listening to port 3000. Updated server.ts to import front_end_app.js and created homepage.ejs

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,13 @@
 import app from "./app";
+import front_end_app from "../front_end/front_end_app";
 
 const PORT = 8765;
+const PORT_front = 3000;
 
 app.listen(PORT, ()=> {
     console.log('Express server listening on port ' + PORT);
+})
+
+front_end_app.listen(PORT_front, ()=> {
+    console.log('Client is listening on port ' + PORT_front)
 })
