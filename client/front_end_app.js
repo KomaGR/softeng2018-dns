76cc6bee3f8b8572f5d415a0e6699a2451b5d8ce
GitZonk <vassilis_mitropoulos@yahoo.gr>
2019-02-24 16:34:39 +0200

    Updated gitignore to correctly NOT ignore js files inside the client folder

---
+++ client/front_end_app.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes_1 = require("../server/routes/routes");
+class Front_end_app {
+    constructor() {
+        this.front_end_app = express();
+        this.config();
+    }
+    config() {
+        this.front_end_app.use(bodyParser.json());
+        this.front_end_app.use(bodyParser.urlencoded({ extended: false }));
+        routes_1.default(this.front_end_app);
+        this.front_end_app.get("/", function(req, res){
+            res.render("../client/homepage.ejs");
+        });
+    }
+}
+exports.default = new Front_end_app().front_end_app;
+//# sourceMappingURL=app.js.map
