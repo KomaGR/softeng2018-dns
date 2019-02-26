30dc85ac0d4800c5764a8c1851f009539d4e09c8
GitZonk <vassilis_mitropoulos@yahoo.gr>
2019-02-26 14:35:46 +0200

    Add functionality to searching products or categories (with mock up response)

--- client/front_end_app.js
+++ client/front_end_app.js
@@ -15,22 +15,51 @@ class Front_end_app {
         this.front_end_app.use(bodyParser.json());
         this.front_end_app.use(bodyParser.urlencoded({ extended: false }));
         routes_1.default(this.front_end_app);
+
         this.front_end_app.get("/", function(req, res){
             res.render("../client/pages/homepage.ejs");
         });
+
         this.front_end_app.get("/search_results", function(req, res){
             var searchTerm = req.query.search_term;
-            res.render("../client/pages/search_results.ejs");
+            const options = {
+                hostname: 'localhost',
+                port: 8765,
+                path: '/observatory/api/products',
+                rejectUnauthorized: false,
+                method: 'GET'
+            };
+            const httpsreq = https.request(options, (httpsres)=> {
+                console.log('statuscode', httpsres.statusCode);
+                httpsres.on('data', (d) => {
+                    var mydata =  JSON.parse(d);
+                    var myproducts = mydata.products.filter(function (entry){
+                        return ( entry.name.toLowerCase().includes(searchTerm.toLowerCase()) || 
+                        entry.category.toLowerCase().includes(searchTerm.toLowerCase()) );
+                    });
+                    res.render("../client/pages/search_results.ejs", {
+                        myproducts: myproducts
+                    });
+                });
+            });
+            httpsreq.on('error', (e)=> {
+                console.error(e);
+            });
+            httpsreq.end();
         });
+
         this.front_end_app.post("search_results", function(req, res){
             res.redirect('/product_info');
         });
+
         this.front_end_app.get("/about", function(req, res){
             res.render("../client/pages/about.ejs");
         });
+
         this.front_end_app.get("/login", function(req, res){
             res.render("../client/pages/login.ejs");
         });
+
         this.front_end_app.post("/login", function(req, res){
             var username = req.body.l_username;
             var password = req.body.l_password;
@@ -59,9 +88,11 @@ class Front_end_app {
             });
             httpsreq.end();
         });
+
         this.front_end_app.get("/sign_up", function(req, res){
             res.render("../client/pages/sign_up.ejs");
         });
+
         this.front_end_app.post("/sign_up", function(req, res){
             var userEmail = req.body.s_email;
             var username = req.body.s_username;
@@ -94,18 +125,23 @@ class Front_end_app {
             });
             httpsreq.end();
         });
+
         this.front_end_app.get("/submit_product", function(req, res){
             res.render("../client/pages/submit_product.ejs");
         });
+
         this.front_end_app.post("/submit_product", function(req, res){
             res.redirect("/submit_shop");
         });
+
         this.front_end_app.get("/product_info", function(req, res){
             res.render("../client/pages/product_info.ejs");
         });
+
         this.front_end_app.get("/submit_shop", function(req, res){
             res.render("../client/pages/submit_shop.ejs");
         });
+
         this.front_end_app.post("/submit_shop", function(req, res){
             res.redirect("/submit_product");
         });
