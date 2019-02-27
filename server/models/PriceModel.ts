ed8da78afe1d4ff7d366798806c0d032edd64b44
steliostss <tsagarakis.stelios@gmail.com>
2019-02-27 23:14:29 +0200

    Fix Models and Controllers

---
+++ server/models/PriceModel.ts
@@ -0,0 +1,31 @@
+import * as mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+
+export const PriceSchema = new Schema({
+    price: {
+        type: Number,
+        required: 'Enter price'
+    },
+    dateFrom: {
+        type: Date,
+        default: Date.now
+    },
+    dateTo: {
+        type: Date,
+        default: Date.now
+    },
+    productId: {
+        type: String,
+        required: 'Enter productId'
+    },
+    shopId: {
+        type: String,
+        required: 'Enter shopId'
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    }
+});
