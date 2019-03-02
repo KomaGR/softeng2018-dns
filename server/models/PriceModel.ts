568187ce8f999be0d3c7e6726fc782aa25c5de3c
steliostss <tsagarakis.stelios@gmail.com>
2019-03-03 01:12:21 +0200

    Commit before rebase

--- server/models/PriceModel.ts
+++ server/models/PriceModel.ts
@@ -1,24 +1,31 @@
 import * as mongoose from 'mongoose';
+import { ProductSchema } from './ProductModel';
+import { ShopSchema } from './ShopModel';
 
 const Schema = mongoose.Schema;
+
+const Product = mongoose.model('Product', ProductSchema);
+const Shop = mongoose.model('Shop', ShopSchema);
 
 export const PriceSchema = new Schema({
     price: {
         type: Number,
         required: 'Enter price'
     },
-    date: {
+    dateFrom: {
         type: Date,
         required: 'Enter date',
         default: Date.now,
     },
     productId: {
-        type: String,
-        required: 'Enter productId'
+        type: Schema.Types.ObjectId,
+        required: 'Enter productId',
+        ref: 'Product'
     },
     shopId: {
-        type: String,
-        required: 'Enter shopId'
+        type: Schema.Types.ObjectId,
+        required: 'Enter shopId',
+        ref: 'Shop'
     },
     dateCreated: {
         type: Date,
