beb8394470593f35cd3af83106c946466cadb333
steliostss <tsagarakis.stelios@gmail.com>
2019-03-02 07:59:43 +0200

    Correct multiple price insertion, problem with correct date remains

--- server/controllers/PriceController.ts
+++ server/controllers/PriceController.ts
@@ -1,24 +1,37 @@
 import * as mongoose from 'mongoose';
 import * as PriceModel from '../models/PriceModel';
 import * as express from 'express';
+let DateDiff = require('date-diff');
 
 type Request = express.Request;
 type Response = express.Response;
 
-const Price = mongoose.model('Price', PriceModel.PriceSchema);
+export const Price = mongoose.model('Price', PriceModel.PriceSchema);
 
 export class PriceController {
 
-    // add a new price on database
     public addNewPrice(req: Request, res: Response) {
-        let newPrice = new Price(req.body);
+        let date1 = new Date(req.body.dateFrom);
+        let date2 = new Date(req.body.dateTo);
 
-        newPrice.save((err, price) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json(price);
-        });
+        let diff = new DateDiff(date2, date1);
+        let from = date1;
+
+        console.log(diff.days(), typeof (diff.days()));
+
+        for (let i = 0; i <= diff.days(); i++) {
+            req.body.dateTo = i+1;
+            let newPrice = new Price(req.body);
+
+            newPrice.save((err, price) => {
+                if (err) {
+                    res.send(err);
+                }
+                // res.json(price);
+            });
+        }
+        res.sendStatus(200)
+
     }
 
     // get all prices (according to query) from database
@@ -33,7 +46,7 @@ export class PriceController {
 
       public getPriceWithID(req: Request, res: Response) {
         Price.findById(
-            { _id: req.params.priceId },
+            { id: req.params.priceId },
             (err, price) => {
                 if (err) {
                     res.send(err);
