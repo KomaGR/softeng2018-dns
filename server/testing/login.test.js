026419cf625e38f99e04ee155c99fcb7d6f406ab
Orestis Kaparounakis <orestis.kapar@hotmail.com>
2019-03-04 07:22:50 +0200

    Add login intergration tests
    Note: Test named 'POST /signup existing user fails' seems to fail when
    tested in par with the others in the file but succeeds when alone. This
    would make sense **if** the database was not cleared after each test
    *(which it is)* **and** was using identical credentials with some of the
    other tests.

--- server/testing/login.test.js
+++ server/testing/login.test.js
@@ -1,8 +1,11 @@
 const app = require("../app");
 const request = require('supertest');
+const async = require('async');
 
 
-describe('Test the root path', () => {
+
+
+describe('Login endpoints', () => {
 
     var server;
 
@@ -18,14 +21,67 @@ describe('Test the root path', () => {
         server.close();
     });
 
-    test('Responds at products', (done) => {
-        request(server).get('/observatory/api/products').expect(200);
-        done();
+    beforeEach((done) => {
+        app.default.db.dropDatabase( () => {
+            done();
+        });
     });
 
-    test('Responds at shops', (done) => {
-        request(server).get('/observatory/api/shops').expect(200);
-        done();
+    test('Empty POST /login is Bad Request', (done) => {
+        request(server)
+        .post('/observatory/api/login')
+        .then((res) => {
+            expect(res.statusCode).toBe(400);
+            done();
+        });
     });
 
+    test('POST /signup new user', (done) => {
+        request(server)
+        .post('/observatory/api/signup')
+        .send({email: 'username@example.com', username: 'testname', password: '12345'})
+        .set('Accept', 'application/json')
+        .expect(201, done);
+    });
+    
+    test('POST /login existing user', (done) => {
+        async.waterfall([
+            (cb) => request(server)
+                    .post('/observatory/api/signup')
+                    .send({email: 'username@example', username: 'testname', password: '12345'})
+                    .set('Accept', 'application/json')
+                    .expect(201, cb),
+            (results, cb) => request(server)
+                .post('/observatory/api/login')
+                .send({username: 'testname', password: '12345'})
+                .set('Accept', 'application/json')
+                .expect(200, cb)
+        ], (err, res) => {
+            if (err) done(err);
+            expect(res.body.token).toBeDefined();
+            expect(res.body.token).toBeString();
+            
+            done();
+        });
+    });
+
+    // TODO: Something is amiss with this test.
+    test('POST /signup existing user fails', (done) => {
+        async.waterfall([
+            (cb) => request(server)
+                    .post('/observatory/api/signup')
+                    .send({email: 'username2@example.com', username: 'testname2', password: '12345'})
+                    .set('Accept', 'application/json')
+                    .expect(201, cb),
+            (results, cb) => request(server)
+                .post('/observatory/api/signup')
+                .send({email: 'username2@example.com', username: 'testname2', password: '12345'})
+                .set('Accept', 'application/json')
+                .expect(403, cb)
+        ], (err, res) => {
+            if (err) done(err);
+            done();
+        });
+    });
+    
 });
