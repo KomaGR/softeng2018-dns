e4e43806b4f494ee6d47373227dcfd771b966a56
steliostss <tsagarakis.stelios@gmail.com>
2019-03-04 04:46:25 +0200

    Add UserRouter

---
+++ server/routes/UserRouter.ts
@@ -0,0 +1,46 @@
+import * as express from 'express';
+import { UserController } from '../controllers/UserController';
+import { request } from 'http';
+
+type Request = express.Request;
+type Response = express.Response;
+
+/*  Note:   This class is subject to later abstraction via passing
+            {method, path, callee} vectors. Hence the warning 
+            below.
+*/
+export default class {
+
+    public router: express.Router;
+    public userController: UserController = new UserController();
+
+    constructor() {
+        this.router = express.Router();
+        this.config();
+    }
+
+    // requests not handled here
+    // dispatched to appropriate files
+
+    private config(): void {
+        this.router
+            // get all users
+            // .get('/', this.userController.getProduct)
+
+            // create a new user
+            .post('/', this.userController.addNewUser)
+
+            // get a specific user
+            .get('/:id', this.userController.getUserWithID)
+
+            // update a specific user
+            .put('/:id', this.userController.updateUser)
+
+            // update only one field of a specific user
+            .patch('/:id', this.userController.partialUpdateUser)
+
+            // delete a specific user
+            .delete('/:id', this.userController.deleteUser)
+    }
+
+}
